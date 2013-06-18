c5c24790d4aebb55116055d7154db99973dabd70
wengyouran <wengyouran40@hotmail.com>
2013-06-18 00:27:56 -0700

    initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Karla::Application.config.secret_token = 'dbcfd533ef9b07ed757d90fcadb6086d7f468806724316ee8d0915a630b5f4070ec3c27b8d507d2c92b11b50242a5953cabee58fd4ab52e5ee716ac2eafe2af5'
